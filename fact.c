b854b2a79865d0d1fcb4d43260d7a5bd743e2143
Shubhi251810 <shubhichaudhary822@gmail.com>
2025-01-30 09:40:53 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,16 @@
+//factorial of number.
+#include<stdio.h>
+int main()
+{
+	int n=5,i,fact=1;
+	
+	 for(i=1;i<=n;i++)
+	 { 
+	 	fact=fact*i;
+	 	
+	 }
+    
+	 printf("%d",fact);
+    
+	 return 0;
+}
