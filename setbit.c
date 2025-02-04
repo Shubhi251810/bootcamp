f4f2d6ec32f34d10c91369c073045d51ff1ea20a
Shubhi251810 <shubhichaudhary822@gmail.com>
2025-02-04 10:07:29 +0530

    Add files via upload

---
+++ setbit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,k;
+	scanf("%d",&n);
+	scanf("%d",&k);
+	int a=1&(n>>k);
+	if(a==1)
+	{
+		printf("bit is set");
+	}
+	else
+	{
+		printf("bit is clear");
+	}
+	return 0;
+}
