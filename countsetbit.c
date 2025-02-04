2a7bc945f650d7e193b122e64a06267bef031629
Shubhi251810 <shubhichaudhary822@gmail.com>
2025-02-04 12:05:08 +0530

    Add files via upload

---
+++ countsetbit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int countsetbit(int n)
+{
+	int count=0;
+	while(n>0)
+	{
+		n=n&(n-1);
+		count=count+1;
+	}
+	return count;
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d",countsetbit(n));
+	return 0;
+}
